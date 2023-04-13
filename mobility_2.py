587246e5b7a83413f3c195902049475ceeede312
eduardogama <eduardogama72@gmail.com>
2023-04-13 15:58:46 -0300

    Updates 4/13/23

--- mobility_2.py
+++ mobility_2.py
@@ -4,6 +4,10 @@
 
 import sys
 
+from time import sleep
+
+from mn_wifi.node import UserAP
+from mininet.node import Controller
 from mininet.log import setLogLevel, info
 from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
@@ -14,10 +18,13 @@ from mininet.term import makeTerm
 
 def topology(args):
     "Create a network."
-    net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
+    net = Mininet_wifi(controller=Controller, link=wmediumd, accessPoint=UserAP,
+                       wmediumd_mode=interference)
 
-    sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', min_x=100, max_x=2000, min_y=100, max_y=1400, min_v=10, max_v=20)
-    sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', min_x=100, max_x=2000, min_y=100, max_y=1400, min_v=3, max_v=5)
+    sta1 = net.addStation('sta1', mac='00:00:00:00:00:01', range=10,
+                          min_x=100, max_x=2000, min_y=100, max_y=1400, min_v=10, max_v=0)
+    sta2 = net.addStation('sta2', mac='00:00:00:00:00:02', range=10,
+                          min_x=100, max_x=2000, min_y=100, max_y=1400, min_v=3, max_v=5)
     # sta3 = net.addStation('sta3', mac='00:00:00:00:00:03', min_x=100, max_x=700, min_y=50, max_y=450, min_v=20, max_v=30)
     # sta4 = net.addStation('sta4', mac='00:00:00:00:00:04', min_x=100, max_x=700, min_y=50, max_y=450, min_v=20, max_v=30)
     # sta5 = net.addStation('sta5', mac='00:00:00:00:00:05', min_x=100, max_x=700, min_y=50, max_y=450, min_v=20, max_v=30)
@@ -26,9 +33,9 @@ def topology(args):
 
     kwargs = {'mode': 'g', 'failMode': 'standalone'}
 
-    e1 = net.addAccessPoint('e1', mac='00:00:00:11:00:01', channel='1',
+    e1 = net.addAccessPoint('e1', mac='00:00:00:11:00:01', channel='1', 
                             position='400,1050,0', ssid='ssid-ap1', **kwargs)
-    e2 = net.addAccessPoint('e2', mac='00:00:00:11:00:02', channel='1',
+    e2 = net.addAccessPoint('e2', mac='00:00:00:11:00:02', channel='1', 
                             position='1000,1050,0', ssid='ssid-ap2', **kwargs)
     e3 = net.addAccessPoint('e3', mac='00:00:00:11:00:03', channel='1',
                             position='1600,1050,0', ssid='ssid-ap3', **kwargs)
@@ -57,7 +64,7 @@ def topology(args):
 
     net.addLink(e1, h1)
 
-    net.setMobilityModel(time=0, model='RandomDirection', max_x=2000, max_y=1200, seed=20)
+    net.setMobilityModel(time=10, model='RandomDirection', max_x=2000, max_y=1200, seed=20)
 
 
     net.plotGraph(max_x=2000, max_y=1600)
@@ -74,9 +81,12 @@ def topology(args):
     e5.start([])
     e6.start([])
     
-    sta1.cmd('google-chrome --no-sandbox http://143.106.73.50:30002/samples/ericsson/vod-1.html')
+    # sta1.cmd('google-chrome --no-sandbox http://143.106.73.50:30002/samples/ericsson/vod-1.html')
     # sta1.cmd('python end-user/run-player-main.py')
-    # makeTerm(sta1, cmd="python end-user/run-player-main.py")
+    # makeTerm(sta1, cmd="python end-user/run-player-main-2.py")
+    # sleep(2)
+    # makeTerm(sta1, cmd='google-chrome --no-sandbox http://143.106.73.50:30002/samples/ericsson/vod-1.html')
+
     # makeTerm(sta2, cmd="python end-user/run-player-main.py")
     # makeTerm(sta2, cmd="bash -c 'python end-user/run-player-main.py'")
 
