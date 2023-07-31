91e1addb64ea4f03583420dba1d6fea6b6f319fa
eduardogama <eduardogama72@gmail.com>
2023-07-31 00:04:57 -0300

    Updates 7/31/23: Updating prepareLog.py file

--- basic/prepareLog.py
+++ basic/prepareLog.py
@@ -1,47 +1,47 @@
 import os
+import sys
 import shutil
 
 
-print("Directory name [Default: users/]:")
-dir_name = str(input())
 
-dir_name = 'users' if dir_name == "" else dir_name
-
-print("Number of seeds[Default: 10]:")
-seeds = str(input())
-
-seeds = 10 if seeds == "" else int(seeds)
-
-
-for s in range(seeds):
-    location = dir_name + '/' + str(s) + '/'
-    users = [ x for x in os.listdir(location) if os.path.isdir(location + x)] 
+def main():
+    if len(sys.argv) < 1:
+        print("python prepareLog.py <dir name> <number of seeds>") 
+        return 
     
-    
-    for u in users:
-    
-        newfile = open(location + u + '-player.csv','w')
-        newfile.write('K liveQoE videoQuality videoQualitySwitch StallsDuration bufferLevel throughput\n')
+    dir_name = sys.argv[1]
+    seeds = int(sys.argv[2])
 
-        pathfile = location + u + '/Default/chrome_debug.log'
+
+    for s in range(seeds):
+        location = dir_name + '/' + str(s) + '/'
+        users = [ x for x in os.listdir(location) if os.path.isdir(location + x)] 
         
-        with open(pathfile) as f:
-            lines = f.readlines()
-            
-            count = 0
-            for line in lines:
-                cells = line.split('"')
-                print(cells)
-                if not "http://143.106.73.50:30002/samples/ericsson/js/main.js" in cells[-1] or cells[1] == "ok":
-                    continue
-                
-                if count < 2:
-                    count += 1
-                    continue
-                    
-                newfile.write(cells[1] + "\n")
-    
-    newfile.close()
+        
+        for u in users:
+        
+            newfile = open(location + u + '-player.csv','w')
+            newfile.write('K liveQoE videoQuality videoQualitySwitch StallsDuration bufferLevel throughput\n')
 
-#    shutil.rmtree(location)
-    
+            pathfile = location + u + '/Default/chrome_debug.log'
+            
+            with open(pathfile) as f:
+                lines = f.readlines()
+                
+                count = 0
+                for line in lines:
+                    cells = line.split('"')
+                    print(cells)
+                    if not "http://143.106.73.50:30002/samples/ericsson/js/main.js" in cells[-1] or cells[1] == "ok":
+                        continue
+                    
+                    if count < 2:
+                        count += 1
+                        continue
+                        
+                    newfile.write(cells[1] + "\n")
+        
+        newfile.close()
+
+if __name__ == '__main__':
+    main()
