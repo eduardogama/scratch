c11c7f86c3bdb852be999d43ed400d2a8ec8ead6
eduardogama <eduardogama72@gmail.com>
2023-08-10 18:07:28 -0300

    Updates

--- basic/local-sim-position.py
+++ basic/local-sim-position.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 """Setting the position of nodes and providing mobility"""
-from pyvirtualdisplay import Display
-
-import sys
 import os
+import sys
 import math
 import json
 import random
@@ -23,24 +21,6 @@ from mininet.term import makeTerm
 
 
 
-#display = Display(visible=0, size=(1024, 768))
-#display = Display()
-#display.start()
-
-### Run this code in orchestrator server ###
-#import requests
-#requests.post('http://localhost:30600/bsPlacement',
-#               json={
-#                     'BS-1': 'http://143.106.73.17:30001',
-#                     'BS-2': 'http://143.106.73.17:30001',
-#                     'BS-3': 'http://143.106.73.23:30001',
-#                     'BS-4': 'http://143.106.73.23:30001',
-#                     'BS-5': 'http://143.106.73.19:30001',
-#                     'BS-7': 'http://143.106.73.23:30001',
-#                     'BS-8': 'http://143.106.73.19:30001',
-#                     'Cloud-1': 'http://143.106.73.50:30002',
-#               })
-
 
 def next_time(rateParameter: float, RAND_MAX: int = 0):
     return -math.log(1.0 - random.random() / (RAND_MAX + 1)) / rateParameter
@@ -54,7 +34,6 @@ def DashPlayer(stas: object):
         sleep(val)
         print(sta.wintfs[0].ip, "starting video ... ", sta.wintfs[0].ssid)
         
-#        sta.cmd('python dash-emulator.py http://143.106.73.17:30001/akamai/bbb_30fps/bbb_30fps.mpd')
         makeTerm(sta, cmd='python dash-emulator.py http://143.106.73.17:30001/akamai/bbb_30fps/bbb_30fps.mpd')
 
 
@@ -67,12 +46,10 @@ def ChromePlayer(stas: object, abrStrategy: str):
         makeTerm(
             sta, cmd='google-chrome '
                      '--headless '
-#                     '--disable-application-cache '
-#                     '--media-cache-size=1 '
-                     '--disk-cache-dir=/dev/null '
                      '--no-sandbox '
                      '--incognito '
                      '--new-window '
+                     '--disk-cache-dir=/dev/null '
                      'http://143.106.73.50:30002/samples/ericsson/vod-client.html?userid={}&abrStrategy={}'.format(sta.name, abrStrategy))
 
 
@@ -83,40 +60,11 @@ def SeleniumPlayer(stas: object, abrStrategy: str, count: str):
         sleep(val)
         print(sta.wintfs[0].ip, "starting video ... ", sta.wintfs[0].ssid)
         
-        makeTerm(sta, cmd='python run-player-main.py {} {} {}'.format(abrStrategy, sta.name, count))
+        makeTerm(sta, cmd='python run-player-main.py abrDynamic {} {}'.format(sta.name, count))
         
         
 def incoming(stas: object, abrStrategy: str, count: str):
     SeleniumPlayer(stas, abrStrategy, count)
-
-
-def monitoring(stas):
-    # Monitor the connectivity of the station
-    prev_ap = np.array([None for i in enumerate(stas)])
-
-    
-    for k in range(350):
-        for i, sta in enumerate(stas):
-            connected_ap = sta.wintfs[0].ssid
-
-            if connected_ap != prev_ap[i]:
-                print(
-                    json.dumps({
-                        "userName": sta.name, 
-                        "bsName": connected_ap, 
-                        "ip": sta.wintfs[0].ip
-                    })
-                )
-
-                os.system(
-                    "curl -X POST 143.106.73.50:30700/collect/handover -H 'Content-Type: application/json' -d $(jo "
-                    "userName={} bsName={} ip={})"
-                    .format(sta.name, connected_ap, sta.wintfs[0].ip))
-
-                prev_ap[i] = connected_ap
-            
-        print(2*k, end=' ', flush=True)
-        sleep(2)
 
 
 def topology(args):
@@ -127,8 +75,8 @@ def topology(args):
     abrStrategy = sys.argv[1] if len(sys.argv) > 1 else "abrDynamic"
     nusers = int(sys.argv[2]) if len(sys.argv) > 2 else 5
 
-    pos1 = sys.argv[3] if len(sys.argv) > 3 else '500.0,1050.0,0.0'
-    pos2 = sys.argv[4] if len(sys.argv) > 3 else '500.0,1050.0,0.0'
+    pos1 = sys.argv[3] if len(sys.argv) > 3 else '401.0,1050.0,0.0'
+    pos2 = sys.argv[4] if len(sys.argv) > 3 else '401.0,1050.0,0.0'
 
     count = sys.argv[5] if len(sys.argv) > 3 else '0'
 
@@ -160,8 +108,8 @@ def topology(args):
     net.addLink(e1, e2)
     net.addLink(e2, e3)
 
-#    if '-p' not in args:
-#        net.plotGraph(max_x=2000, max_y=2000)
+    if '-p' not in args:
+        net.plotGraph(max_x=2000, max_y=2000)
 
     info("*** Starting network\n")
     net.build()
@@ -171,13 +119,7 @@ def topology(args):
     e2.start([])
     e3.start([])
 
-    # stations = np.array([sta1, sta2])
-    stations = np.array(net.stations)
-#    threading.Thread(target=monitoring, args=(stations,)).start()
-    threading.Thread(target=incoming, args=(stations,abrStrategy, count)).start()
-
-#    CLI(net)
-    monitoring(stations)
+    CLI(net)
 
     info("*** Stopping network\n")
     net.stop()
@@ -187,5 +129,3 @@ if __name__ == '__main__':
     setLogLevel('info')
     topology(sys.argv)
 
-
-#display.stop()
